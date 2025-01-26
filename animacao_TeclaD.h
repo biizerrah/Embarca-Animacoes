a3c4d49bc33fd2c0d61fdc27d7346a0b5e4dab22
Matuzalém Guimarães <matuinfo@gmail.com>
2025-01-26 16:22:51 -0300

    features-buzzer-teste-reset

--- animacao_TeclaD.h
+++ animacao_TeclaD.h
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "matriz_rgb.h"
+#include "buzzer.h"
+
 
 // Função para ligar todos os LEDs na cor verde (50% de intensidade)
 void animacao_TeclaD() {
+        play_sound(21, 1000, 300); // Emite um som de 1 kHz por 500 ms no pino GPIO 21
+        
     // Define a cor verde com 50% de intensidade (R = 0, G = 127, B = 0)
     int intensidade_verde = 127; // 50% de 255 = 127
 
--- animacao_TeclaHastag.h
+++ animacao_TeclaHastag.h
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "matriz_rgb.h"
+#include "buzzer.h"
 
 // Função para ligar todos os LEDs na cor branca (20% de intensidade)
 void animacao_TeclaHashtag() {
+    play_sound(21, 1000, 300); // Emite um som de 1 kHz por 500 ms no pino GPIO 21
+    
     // Define a cor branca com 20% de intensidade (R = G = B = 51)
     int intensidade_branca = 51; // 20% de 255 = 51
 
